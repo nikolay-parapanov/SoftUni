25f5e4c18deefe1d3648b2ec0093766470046081
Nikolay Parapanov <n.parapanoff@gmail.com>
2023-04-02 23:40:50 +0300

    last objects and classes problems from the exercise

---
+++ 08-JS/03-objects-and-classes/10-vehicle.js
@@ -0,0 +1,18 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power,
+        }
+        this.fuel = fuel;
+    }
+
+    drive(fuel){
+        this.fuel -= fuel; 
+
+    }
+
+}
